4db15759dd340304a33862ea8737513dbde6dc97
Ddeathtrapp <n-ali7@hvcc.edu>
2025-12-03 21:29:34 -0500

    Codex Update

---
+++ app/build/generated/source/buildConfig/tv/debug/com/mirror/tvreceiver/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mirror.tvreceiver;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mirror.tvreceiver";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "tv";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String SIGNALING_URL = "ws://192.168.1.100:8080";
+}
